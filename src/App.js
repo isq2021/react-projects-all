a0d9c04aa78a76bd6ddd5c4b37c31690da48829f
Isku Esenov <snvislm2021@gmail.com>
2022-02-21 18:16:47 +0600

    movie api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from 'react';
+import "./App.css"
+import ModalPage from './components/ModalPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      actors :[],
+      isTheModalOn:false,
+      selectedActor:[]
+
+    }
+  }
+
+  async componentDidMount(){
+    const URL ='https://api.themoviedb.org/3/person/popular?api_key=b476299faf4876eeee6c13a37ea60d0c&language=en'
+    
+    try {
+      
+    const resp =  await fetch(URL)
+    const data =await resp.json()
+    this.setState({actors:data.results})
+    
+
+    }catch(err){
+      console.log(err)
+
+    }
+  
+
+  }
+
+  toggleModal(id){
+    this.setState({isTheModalOn:true})
+    const selectedActor =  this.state.actors.filter((actor)=> actor.id === id)
+    this.setState({selectedActor})
+  }
+
+  handalModal = (status) =>{
+    this.setState({isTheModalOn:status})
+  }
+  
+  render() {
+    const {actors} =this.state
+    return (
+      <>
+      <div className='app'>
+        <h1>ACTORS</h1>
+        <div className='movies' >
+          {actors.map((actor) => {
+            const {name,profile_path,id} = actor
+            return (
+            <div className='actor' onClick={()=>this.toggleModal(id)}>
+              <figure>
+                <img src={'https://image.tmdb.org/t/p/w500/' + profile_path} className='image-actor'alt='actor' />
+              </figure>
+              <h3>{name}</h3>
+              
+              {actor.known_for.map((e)=>{
+                return (
+                  <h6>
+                    {e.title}
+                    
+                  </h6>
+
+
+                )
+
+              })}
+
+             
+              
+              
+
+
+
+            </div>
+            )
+            
+          })}
+
+        </div>
+        {this.state.isTheModalOn ? <ModalPage handalModal={this.handalModal} dataActor={this.state.selectedActor} /> : null}
+      </div>
+        
+      </>
+    );
+  }
 }
 
 export default App;
