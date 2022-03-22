7eca2f2ae1ddd3d5dde2d6cc6448af8c2f29e8b9
Isku Esenov <snvislm2021@gmail.com>
2022-03-22 18:14:40 +0600

    ads

--- use-reducer/src/Context/GeneralContext.jsx
+++ use-reducer/src/Context/GeneralContext.jsx
@@ -3,21 +3,6 @@ import { v4 as uuidv4 } from "uuid";
 
 export const GeneralContext = createContext([]);
 
-const initialTracker = [];
-const reduce = (state, action) => {
-  switch (action.type) {
-    case "addTransaction":
-      return [...state, { ...action.payload, id: uuidv4() }];
-
-    case "deleteTransaction":
-      return state.filter((x) => {
-        return x.id !== action.payload;
-      });
-    default:
-      return state;
-  }
-};
-
 export const GeneralProvider = ({ children }) => {
   const [formData, setFormData] = useState({ type: "Income", value: 0 });
   const [transactions, dispatch] = useReducer(reduce, initialTracker);
