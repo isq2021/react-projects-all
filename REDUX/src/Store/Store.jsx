3e9dc9690576f4a6914385c9110f9d721071f044
Isku Esenov <snvislm2021@gmail.com>
2022-03-30 12:44:36 +0600

    as

--- REDUX/src/Store/Store.jsx
+++
@@ -1,8 +0,0 @@
-import { createStore, combineReducers } from "redux";
-import counterReducer from "./Reducers/CounterReducer";
-import todosReducer from "./Reducers/TodosReducer";
-
-const allReducers = combineReducers({ counterReducer, todosReducer });
-
-const store = createStore(allReducers);
-export { store };
