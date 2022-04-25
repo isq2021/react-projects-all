a35daf1d86730dc165351f28f20777f98b2ec2da
Isku Esenov <snvislm2021@gmail.com>
2022-04-25 20:23:37 +0600

    as

--- gallery/src/App.js
+++ gallery/src/App.js
@@ -1,35 +1,64 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { getPhotos } from "./galleryState";
 import ClassComponent from "./classComponent";
 
+import ReactPaginate from "react-paginate";
+
 function App() {
   const dispatch = useDispatch();
   const photos = useSelector((state) => state.gallery.photos);
+
   useEffect(() => {
     dispatch(getPhotos());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dispatch]);
-  console.log(photos, "photos");
+
+  const [pageNumber, setPageNumber] = useState(0);
+  const picsPerPage = 10;
+  const pagesVisited = pageNumber * picsPerPage;
+  const slicedPhotos = photos.slice(pagesVisited, pagesVisited + picsPerPage);
+
+  const displayPICS = slicedPhotos.map((photo) => {
+    return (
+      <>
+        <img
+          alt={photo.author}
+          key={photo.id}
+          src={photo.download_url}
+          width="200"
+          height="200"
+        />
+      </>
+    );
+  });
+
+  const pageCount = Math.ceil(photos.length / picsPerPage);
+  const handlePageClick = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
   return (
     <>
       <div className="App">
-        <h1>POTO GALLERY</h1>
-        <p>Enjoy!</p>
-        <hr />
-        <div className="gallery">
-          {photos.map((photo) => (
-            <img
-              alt={photo.author}
-              key={photo.id}
-              src={photo.download_url}
-              width="300"
-              height="300"
-            />
-          ))}
-        </div>
-        <button>VIEW MORE</button>
-      </div>
+        <h1> POTO GALLERY </h1> <p> Enjoy! </p> <hr />
+        <div className="gallery">{displayPICS}</div>{" "}
+      </div>{" "}
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel="next >"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={pageCount}
+        previousLabel="< previous"
+        renderOnZeroPageCount={null}
+        containerClassName={"paginationBttns"}
+        previousLinkClassName={"previousBttn"}
+        nextLinkClassName={"nextBttn"}
+        disabledClassName={"paginationDisabled"}
+        activeClassName={"paginationActive"}
+      />
       <ClassComponent />
     </>
   );
